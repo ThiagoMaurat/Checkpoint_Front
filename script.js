17a164bfe371aba30621636e290ea4c13da1326b
Thiago Maurat <thiagomaurat@hotmail.com>
2021-11-03 16:29:43 -0300

    sobe

---
+++ script.js
@@ -0,0 +1,7 @@
+const ir = document.getElementById (Botão);
+const input1 = document.getElementById (input1);
+const input2 = document.getElementById (input2);
+const input3 = document.getElementById (input3);
+ir.addEventListener ('click', function () {
+     
+});
