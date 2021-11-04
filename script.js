2d0f4902683dad516e45db1a3b8abd55fa705007
Thiago Maurat <thiagomaurat@hotmail.com>
2021-11-03 22:18:23 -0300

    sobe

--- script.js
+++ script.js
@@ -4,21 +4,30 @@ let input2 = document.getElementById("input2");
 let input3 = document.getElementById("input3");
 let botaozin = document.getElementById("botao");
 
-document.querySelector("#botao").addEventListener("click", function () {
-    let articlezin = document.createElement("article");
-    let h2zin = document.createElement("h2");
-    let pzin = document.createElement("p");
-    let imgzin = document.createElement("img");
+document.querySelector("#botao").addEventListener("click", function (event) {
+  event.preventDefault();
+  let articlezin = document.createElement("article");
+  let h2zin = document.createElement("h2");
+  let pzin = document.createElement("p");
+  let imgzin = document.createElement("img");
+  ir.appendChild(articlezin);
+  h2zin.innerHTML = input1.value;
+  articlezin.appendChild(h2zin);
+  pzin.innerHTML = input2.value;
+  articlezin.appendChild(pzin);
+  imgzin.setAttribute("src", input3.value);
+  articlezin.appendChild(imgzin);
+  console.log("wefwef");
+  articlezin.style.width = "26,5vw"
+  imgzin.style.width = "80%";
+  
+});
 
-    ir.appendChild(articlezin);
+document.querySelector("#apagar").addEventListener("click", (event) => {
+  //remover o ultimo elemento
+  event.preventDefault();
+    let articlezin = document.getElementsByTagName("article");
+    console.log(articlezin);
+    ir.removeChild(articlezin[articlezin.length - 1]);
 
-    h2zin.innerHTML = input1.value;
-    articlezin.appendChild(h2zin);
-
-    pzin.innerHTML = input2.value;
-    articlezin.appendChild(pzin);
-
-    imgzin.setAttribute("src", input3.value);
-    articlezin.appendChild(imgzin);
-    console.log("wefwef");
 });
